011b2f9a6520b21003b03aabd141022eed3dcfd1
sashn <mail.sashman@gmx.net>
2013-10-04 01:30:19 +0200

    no message

--- carousel.js
+++ carousel.js
@@ -17,53 +17,49 @@ var carousel = {
 		'btnLeftSelector': '.js-carousel-btn-left',
 		'btnRightSelector': '.js-carousel-btn-right',
 
-		'itemCount': null,
-		'itemScale': [0.5, 0.7, 1.0, 0.7, 0.5],
+		'visibleItemCount': null,
+		'visibleItemScale': [0.5, 0.7, 1.0, 0.7, 0.5],
 		'itemDefaultWidth': null,
-		'itemDefaultHeight': null
+		'itemDefaultHeight': null,
+		'centerItemIndex': null,
+		'defaultCenterItemIndex': null
 		
 
 	},
 	$items: {},
 	positionCss: {},
+	visibleItemIndices: [],
 
 	init: function(options) {
-		this.options = $.extend({}, this.options, options);
-		this.$items = $(this.options.itemSelector);
-
-		this.recalculateOptions();
-		this.calculateCss();
-		this.initItemPositions();
-		// this.initControls();
+		this.initConfiguration(options);
+		this.calculateCarouselPositionCss();
+		this.adjustItemPositions();
+		this.initControls();
 	},
-	recalculateOptions: function() {
-		var o = this.options;
+
+
+	initConfiguration: function(options) {
+		var o = this.options = $.extend({}, this.options, options);
+
+		this.$items = $(o.itemSelector);
 
 		for (key in o) {
-			if (o[key] === null) {
-				if (key == 'itemCount') {
-					this.options[key] = o.itemScale.length;
-					if (this.options[key]%2 == 0) {
-						throw "number of items should always be uneven!";
-					}
-				} else if (key == 'itemDefaultWidth') {
-					this.options[key] = this.$items.eq(0).width();
-				} else if (key == 'itemDefaultHeight') {
-					this.options[key] = this.$items.eq(0).height();
-				};
+			if (o[key] === null && typeof this.calculateOptions[key] == 'function') {
+				this.options[key] = this.calculateOptions[key](this);
 			};
 		};
 	},
-	calculateCss: function() {
+
+	calculateCarouselPositionCss: function() {
 		var o = this.options
 		,	width
 		,	height
 		,	top
 		,	left;
 
-		for (var i = 0; i < o.itemCount; i++) {
-			width = o.itemScale[i] * o.itemDefaultWidth;
-			height = o.itemScale[i] * o.itemDefaultHeight;
+		for (var i = 0; i < o.visibleItemCount; i++) {
+			width = o.visibleItemScale[i] * o.itemDefaultWidth;
+			height = o.visibleItemScale[i] * o.itemDefaultHeight;
 			top = (o.itemDefaultHeight - height) / 2;
 			left = 0;
 			for (var j = 0; j < i; j++) {
@@ -74,32 +70,86 @@ var carousel = {
 				'height': height,
 				'top': top,
 				'left': left,
-				'z-index': o.itemScale[i]*100
+				'z-index': o.visibleItemScale[i]*100
 			};
 		};
 	},
-	initItemPositions: function() {
+	adjustItemPositions: function() {
+		this.determineVisibleItemIndices();
 		this.$items.hide();
-		for (var i = 0; i < this.options.itemCount; i++) {
-			this.$items.eq(i).css(this.positionCss[i]).show()
+		for (var i = this.visibleItemIndices.length - 1; i >= 0; i--) {
+			this.$items.eq(this.visibleItemIndices[i]).css(this.positionCss[i]).show()
 		};
 	},
 	initControls: function() {
 		var o = this.options
-		,	btnLeft = $(o.btnLeftSelector).get(0)
-		,	btnRight = $(o.btnRightSelector).get(0);
+		,	self = this
+		,	$btnLeft = $(o.btnLeftSelector)
+		,	$btnRight = $(o.btnRightSelector);
 
-		btnLeft.onclick = this.cycleLeft;
-		btnRight.onclick = this.cycleRight;
+		$btnLeft.click(function() {
+			self.cycleLeft(self);
+		});
+		$btnRight.click(function() {
+			self.cycleRight(self);
+		});
 	},
-	cycleLeft: function() {
-		console.log("cycleLeft");
+
+	cycleLeft: function(carousel) {
+		carousel.cycleCarousel(-1);
 	},
-	cycleRight: function() {
-		console.log("cycleRight");
+	cycleRight: function(carousel) {
+		carousel.cycleCarousel(1);
+	},
+	cycleCarousel: function(delta) {
+		this.options.centerItemIndex = this.adjustIndex(this.options.centerItemIndex + delta);
+		this.determineVisibleItemIndices();
+		this.adjustItemPositions();
+	},
+
+	determineVisibleItemIndices: function() {
+		var o = this.options;
+
+		this.visibleItemIndices = [];
+		for (var i = 0; i < o.visibleItemCount; i++) {
+			var index = this.adjustIndex(o.centerItemIndex + i - o.defaultCenterItemIndex);
+			this.visibleItemIndices.push(index);
+		};
+	},
+
+	adjustIndex: function(index) {
+		var itemCount = this.$items.length;
+
+		if (index < 0) {
+			index += itemCount;
+		} else if (index >= itemCount) {
+			index -= itemCount;
+		}
+		return index;
+	},
+
+	calculateOptions: {
+		visibleItemCount: function(self) {
+			var visibleItemCount = self.options.visibleItemScale.length;
+			// if (visibleItemCount%2 == 0) {
+			// 	throw "number of visible items should be uneven!";
+			// }
+			return visibleItemCount;
+		},
+		itemDefaultWidth: function(self) {
+			return self.$items.eq(0).width();
+		},
+		itemDefaultHeight: function(self) {
+			return self.$items.eq(0).height();
+		},
+		centerItemIndex: function(self) {
+			var visibleItemCount = self.options.visibleItemScale.length;
+			return Math.floor(visibleItemCount/2);
+		},
+		defaultCenterItemIndex: function(self) {
+			return this.centerItemIndex(self);
+		}
 	}
-
-
 
 
 
