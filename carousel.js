c225b89c9b54770f05d7a1e05876057061bab854
Sascha van Meurs <s.vanmeurs@reuter.de>
2014-03-13 17:09:42 +0100

    getting better, but also longer :( functionality is good but seems laggy and doesn't react to resizing

--- carousel.js
+++ carousel.js
@@ -7,13 +7,9 @@
 		this.init(element, options);
 	};
 
-	ScrollableTabs.DEFAULTS = {
-	};
-
 	//SPOE =)
 	ScrollableTabs.prototype.init = function(element, options) {
 		this.$container = $(element).addClass('scrollable-tabs-container');
-		this.options = $.extend({}, ScrollableTabs.DEFAULTS, options);
 		this.currentItem = 0;
 
 		// save items, then empty container
@@ -68,28 +64,79 @@
 		});
 	};
 
-	ScrollableTabs.prototype.next = function() {
-		if (this.currentItem < this.$items.length - 1) {
-			var newLeft = this.$items.eq(this.currentItem + 1).position().left;
-			if (newLeft > this.$itemsContainerWidth - this.$viewport.width()) {
-				newLeft = this.$itemsContainerWidth - this.$viewport.width();
-			} else {
-				this.currentItem += 1;
+	ScrollableTabs.prototype.getCurrentItemIndex = function() {
+		var visibleItemIndex = false
+		,	itemsContainerOffset = this.getItemsContainerOffset();
+
+		this.$items.each(function(i) {
+			visibleItemIndex = i;
+
+			//is fully visible?
+			if ($(this).position().left >= itemsContainerOffset) {
+			    return false;
 			}
-			this.$itemsContainer.animate({'left': - newLeft});
+		});
+
+		return visibleItemIndex;
+	};
+
+	ScrollableTabs.prototype.lastItemReached = function(offset) {
+		var viewportWidth = this.$viewport.width()
+		,	itemsContainerWidth = this.$itemsContainer.width()
+		,	offset = offset || this.getItemsContainerOffset();
+
+		return offset + viewportWidth >= itemsContainerWidth;
+	};
+
+	ScrollableTabs.prototype.goToRightBorder = function() {
+		var viewportWidth = this.$viewport.width()
+		,	itemsContainerWidth = this.$itemsContainer.width();
+
+		this.applyOffset(itemsContainerWidth - viewportWidth);
+	};
+
+	ScrollableTabs.prototype.goToItem = function() {
+		
+	};
+
+	ScrollableTabs.prototype.applyOffset = function(offset) {
+		this.$itemsContainer.animate({'left': - offset});
+	};
+
+	ScrollableTabs.prototype.getItemsContainerOffset = function() {
+		return parseInt(this.$itemsContainer.css('left')) * -1;
+	};
+
+	ScrollableTabs.prototype.resizeHandler = function() {
+
+		// if is at end
+
+		// 	go to end
+
+	};
+
+	ScrollableTabs.prototype.next = function() {
+		if (this.lastItemReached()) { //if is at end
+			return false;
+		}
+
+		var nextItemOffset = this.$items.eq(this.getCurrentItemIndex() + 1).position().left;
+
+		if (this.lastItemReached(nextItemOffset)) { //if next step will arrive at end
+			this.goToRightBorder();
+		} else {
+			this.applyOffset(nextItemOffset);
 		}
 	};
 
 	ScrollableTabs.prototype.previous = function() {
-		if (this.currentItem > 0) {
-			var newLeft = this.$items.eq(this.currentItem - 1).position().left;
-			if (newLeft > this.$itemsContainerWidth - this.$viewport.width()) {
-				newLeft = this.$itemsContainerWidth - this.$viewport.width();
-			} else {
-				this.currentItem -= 1;
-			}
-			this.$itemsContainer.animate({'left': - newLeft});
+		if (this.getItemsContainerOffset() <= 0) { //if is at beginning
+			return false;
 		}
+
+		var previousItemOffset = this.$items.eq(this.getCurrentItemIndex() - 1).position().left;
+
+		this.applyOffset(previousItemOffset);
 	};
 
 	$.fn.scrollableTabs = function(option) {
