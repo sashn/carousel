0456e1aa515ddedd5e63b6dc29880aa93ed36a29
Sascha van Meurs <s.vanmeurs@reuter.de>
2014-03-13 17:50:43 +0100

    asd small stuffssssss

--- carousel.js
+++ carousel.js
@@ -10,49 +10,50 @@
 	//SPOE =)
 	ScrollableTabs.prototype.init = function(element, options) {
 		this.$container = $(element).addClass('scrollable-tabs-container');
-		this.currentItem = 0;
 
-		// save items, then empty container
-		if (this.$container.find('.scrollable-tabs-item').length > 0) {
-			var $tempItems = this.$container.find('.scrollable-tabs-item');
-		} else {
-			var $tempItems = this.$container.find('ul li');
-		}
-		this.$container.empty();
-
-		// buttons
-		this.$container.append($('<button class="scrollable-tabs-button-left">prev</button>'));
-		this.$container.append($('<button class="scrollable-tabs-button-right">next</button>'));
-		this.$btnLeft = this.$container.find('.scrollable-tabs-button-left');
-		this.$btnRight = this.$container.find('.scrollable-tabs-button-right');
-
-		// viewport
-		this.$container.append($('<div class="scrollable-tabs-viewport"></div>'));
-		this.$viewport = this.$container.find('.scrollable-tabs-viewport');
-
-		// items
-		this.$viewport.append($('<ul class="scrollable-tabs-items-container"></ul>'));
-		this.$itemsContainer = this.$container.find('.scrollable-tabs-items-container');
-		for (var i = 0, itemCount = $tempItems.length; i < itemCount; i++) {
-			this.$itemsContainer.append($('<li class="scrollable-tabs-item">' + $tempItems.eq(i).html() + '</li>'));
-		};
-		this.$items = this.$container.find('.scrollable-tabs-item');
-
-		// clearfixes
-		this.$container.append($('<div style="clear:both;"></div>'));
-		this.$itemsContainer.append($('<div style="clear:both;"></div>'));
-
-		// set items-container width
-		this.$itemsContainerWidth = 0;
-		for (var i = 0, itemCount = this.$items.length; i < itemCount; i++) {
-			var $tempItem = this.$items.eq(i);
-			this.$itemsContainerWidth += $tempItem.outerWidth() + parseInt($tempItem.css('margin-left')) + parseInt($tempItem.css('margin-right'));
-		};
-		this.$itemsContainer.css({width: this.$itemsContainerWidth});
-
+		this.initMarkup();
+		this.initVariables();
+		this.initItemsContainerWidth();
 		this.initControls();
 	};
 
+	/**
+	 * init functions
+	 */
+	ScrollableTabs.prototype.initMarkup = function() {
+		var markup = ''
+		,	$tempItems = this.$container.find('ul li');
+
+		markup += '<button class="scrollable-tabs-button-left">prev</button>';
+		markup += '<button class="scrollable-tabs-button-right">next</button>';
+		markup += '<div class="scrollable-tabs-viewport">';
+		markup += '<ul class="scrollable-tabs-items-container">';
+		for (var i = 0, itemCount = $tempItems.length; i < itemCount; i++) {
+			markup += '<li class="scrollable-tabs-item">' + $tempItems.eq(i).html() + '</li>';
+		};
+		markup += '<div style="clear:both;"></div>';
+		markup += '</ul>';
+		markup += '</div>';
+		markup += '<div style="clear:both;"></div>';
+
+		this.$container.html(markup);
+	};
+	ScrollableTabs.prototype.initVariables = function() {
+		this.$btnLeft = this.$container.find('.scrollable-tabs-button-left');
+		this.$btnRight = this.$container.find('.scrollable-tabs-button-right');
+		this.$viewport = this.$container.find('.scrollable-tabs-viewport');
+		this.$itemsContainer = this.$container.find('.scrollable-tabs-items-container');
+		this.$items = this.$container.find('.scrollable-tabs-item');
+	};
+	ScrollableTabs.prototype.initItemsContainerWidth = function() {
+		var itemsContainerWidth = 0;
+
+		for (var i = 0, itemCount = this.$items.length; i < itemCount; i++) {
+			itemsContainerWidth += this.$items.eq(i).outerWidth() + parseInt(this.$items.eq(i).css('margin-left')) + parseInt(this.$items.eq(i).css('margin-right'));
+		};
+
+		this.$itemsContainer.css({width: itemsContainerWidth});
+	};
 	ScrollableTabs.prototype.initControls = function() {
 		var self = this;
 
@@ -64,6 +65,36 @@
 		});
 	};
 
+	/**
+	 * public functions
+	 */
+	ScrollableTabs.prototype.next = function() {
+		if (this.lastItemReached()) { //if is at end
+			return false;
+		}
+
+		var nextItemOffset = this.$items.eq(this.getCurrentItemIndex() + 1).position().left;
+
+		if (this.lastItemReached(nextItemOffset)) { //if next step will arrive at end
+			this.goToRightBorder();
+		} else {
+			this.applyOffset(nextItemOffset);
+		}
+	};
+
+	ScrollableTabs.prototype.previous = function() {
+		if (this.getItemsContainerOffset() <= 0) { //if is at beginning
+			return false;
+		}
+
+		var previousItemOffset = this.$items.eq(this.getCurrentItemIndex() - 1).position().left;
+
+		this.applyOffset(previousItemOffset);
+	};
+
+	/**
+	 * private functions
+	 */
 	ScrollableTabs.prototype.getCurrentItemIndex = function() {
 		var visibleItemIndex = false
 		,	itemsContainerOffset = this.getItemsContainerOffset();
@@ -95,48 +126,12 @@
 		this.applyOffset(itemsContainerWidth - viewportWidth);
 	};
 
-	ScrollableTabs.prototype.goToItem = function() {
-		
-	};
-
 	ScrollableTabs.prototype.applyOffset = function(offset) {
-		this.$itemsContainer.animate({'left': - offset});
+		this.$itemsContainer.css({'left': - offset});
 	};
 
 	ScrollableTabs.prototype.getItemsContainerOffset = function() {
 		return parseInt(this.$itemsContainer.css('left')) * -1;
-	};
-
-	ScrollableTabs.prototype.resizeHandler = function() {
-
-		// if is at end
-
-		// 	go to end
-
-	};
-
-	ScrollableTabs.prototype.next = function() {
-		if (this.lastItemReached()) { //if is at end
-			return false;
-		}
-
-		var nextItemOffset = this.$items.eq(this.getCurrentItemIndex() + 1).position().left;
-
-		if (this.lastItemReached(nextItemOffset)) { //if next step will arrive at end
-			this.goToRightBorder();
-		} else {
-			this.applyOffset(nextItemOffset);
-		}
-	};
-
-	ScrollableTabs.prototype.previous = function() {
-		if (this.getItemsContainerOffset() <= 0) { //if is at beginning
-			return false;
-		}
-
-		var previousItemOffset = this.$items.eq(this.getCurrentItemIndex() - 1).position().left;
-
-		this.applyOffset(previousItemOffset);
 	};
 
 	$.fn.scrollableTabs = function(option) {
@@ -158,7 +153,5 @@
 	$.fn.scrollableTabs.Constructor = ScrollableTabs;
 
 }(window.jQuery));
-
-
 
 
