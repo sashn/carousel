1a4bcc4c1808980bb08f48d8f20a39f784bc05d0
Sascha van Meurs <s.vanmeurs@reuter.de>
2014-03-13 09:52:58 +0100

    getting better, functionality present but still buggy

--- carousel.js
+++ carousel.js
@@ -14,6 +14,7 @@
 	ScrollableTabs.prototype.init = function(element, options) {
 		this.$container = $(element).addClass('scrollable-tabs-container');
 		this.options = $.extend({}, ScrollableTabs.DEFAULTS, options);
+		this.currentItem = 0;
 
 		// save items, then empty container
 		if (this.$container.find('.scrollable-tabs-item').length > 0) {
@@ -39,10 +40,19 @@
 		for (var i = 0, itemCount = $tempItems.length; i < itemCount; i++) {
 			this.$itemsContainer.append($('<li class="scrollable-tabs-item">' + $tempItems.eq(i).html() + '</li>'));
 		};
+		this.$items = this.$container.find('.scrollable-tabs-item');
 
 		// clearfixes
 		this.$container.append($('<div style="clear:both;"></div>'));
 		this.$itemsContainer.append($('<div style="clear:both;"></div>'));
+
+		// set items-container width
+		this.$itemsContainerWidth = 0;
+		for (var i = 0, itemCount = this.$items.length; i < itemCount; i++) {
+			var $tempItem = this.$items.eq(i);
+			this.$itemsContainerWidth += $tempItem.outerWidth() + parseInt($tempItem.css('margin-left')) + parseInt($tempItem.css('margin-right'));
+		};
+		this.$itemsContainer.css({width: this.$itemsContainerWidth});
 
 		this.initControls();
 	};
@@ -59,11 +69,27 @@
 	};
 
 	ScrollableTabs.prototype.next = function() {
-		this.$itemsContainer.css({'left': parseInt(this.$itemsContainer.css('left')) - 100});
+		if (this.currentItem < this.$items.length - 1) {
+			var newLeft = this.$items.eq(this.currentItem + 1).position().left;
+			if (newLeft > this.$itemsContainerWidth - this.$viewport.width()) {
+				newLeft = this.$itemsContainerWidth - this.$viewport.width();
+			} else {
+				this.currentItem += 1;
+			}
+			this.$itemsContainer.animate({'left': - newLeft});
+		}
 	};
 
 	ScrollableTabs.prototype.previous = function() {
-		this.$itemsContainer.css({'left': parseInt(this.$itemsContainer.css('left')) + 100});
+		if (this.currentItem > 0) {
+			var newLeft = this.$items.eq(this.currentItem - 1).position().left;
+			if (newLeft > this.$itemsContainerWidth - this.$viewport.width()) {
+				newLeft = this.$itemsContainerWidth - this.$viewport.width();
+			} else {
+				this.currentItem -= 1;
+			}
+			this.$itemsContainer.animate({'left': - newLeft});
+		}
 	};
 
 	$.fn.scrollableTabs = function(option) {
